52cd5392e89a57cc07b76b9b9bb1e33aeb206ede
Taras_Yadzhak <taras.yadzhak@gmail.com>
2019-05-21 19:23:14 +0300

    init

--- src/main/java/com/tiad/App.java
+++ src/main/java/com/tiad/App.java
@@ -1,5 +1,7 @@
+package com.tiad;
+
 public class App {
     public static void main(String[] args) {
-        
+        System.out.println("Hello");
     }
 }
---
+++ src/main/java/com/tiad/ReadWorker.java
@@ -0,0 +1,21 @@
+package com.tiad;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.Queue;
+
+public class ReadWorker implements Runnable {
+
+    private Queue<Line> queue;
+
+    private File file = new File("C:\\"))
+
+    public ReadWorker(Queue<Line> queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
